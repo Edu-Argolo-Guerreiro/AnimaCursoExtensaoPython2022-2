67e2c0f29de14a0f119ad5343b5cb46afd96de4d
Edu Argôlo Guerreiro <105218997+Edu-Argolo-Guerreiro@users.noreply.github.com>
2022-10-20 01:18:39 +0000

    Aprendi a usar print com format e outras formas no Python.

--- main.py
+++ main.py
@@ -4,9 +4,20 @@
 #String = letras, nomes, alfas numeros (frase).
 #Int/Boolean = sem aspas (variavel inteira)
 print("Hello World")
+print("MEU NOME É SÓ EDU, não EDUARDO")
 
 nome = "Edu Guerreiro"
 idade = 18
 
 print(nome)
 print(idade)
+
+#Quando quiser exibir a frase minha "minha idade é:" completando com o conteúdo idade. (Usasse +)
+#Para fazer uma variavel inteira virar string, usasse(+str())
+
+print("Minha idade é:"+str(idade)+"anos")
+print(f"Minha idade é:{idade}\n anos")
+print("Minha idade é:{} anos".format(idade))
+
+#Quando quiser exibir nome e idade pelas variaveis, usasse:
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
