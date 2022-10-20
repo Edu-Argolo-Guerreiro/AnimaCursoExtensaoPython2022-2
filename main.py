3b9fa06147cc60d04d6a76eb3e072847c2475ac4
Edu Argôlo Guerreiro <105218997+Edu-Argolo-Guerreiro@users.noreply.github.com>
2022-10-20 00:53:05 +0000

    Aprendi a fazer variáveis e exibi-la na tela

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # Meu primeiro projeto Python!!!
 # (#) serve para comentario
 # print () = comando de saida
+#String = letras, nomes, alfas numeros (frase).
+#Int/Boolean = sem aspas (variavel inteira)
 print("Hello World")
+
+nome = "Edu Guerreiro"
+idade = 18
+
+print(nome)
+print(idade)
