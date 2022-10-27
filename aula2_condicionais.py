c4690056cd86913619a5888ae35097f20d5d396b
Edu Argôlo Guerreiro <105218997+Edu-Argolo-Guerreiro@users.noreply.github.com>
2022-10-27 01:28:49 +0000

    aula 2 do curso de Python, aprendi sobre condicionais.

---
+++ aula2_condicionais.py
@@ -0,0 +1,22 @@
+# Meu primeiro projeto Python!!!
+# (#) serve para comentario
+# print () = comando de saida
+#String = letras, nomes, alfas numeros (frase).
+#Int/Boolean = sem aspas (variavel inteira)
+print("Hello World")
+
+nome = "Edu Guerreiro"
+idade = 18
+
+print(nome)
+print(idade)
+
+#Quando quiser exibir a frase minha "minha idade é:" completando com o conteúdo idade. (Usasse +)
+#Para fazer uma variavel inteira virar string, usasse(+str())
+
+print("Minha idade é:"+str(idade)+"anos")
+print(f"Minha idade é:{idade}\n anos")
+print("Minha idade é:{} anos".format(idade))
+
+#Quando quiser exibir nome e idade pelas variaveis, usasse:
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
